c4929370953a15857bd449981f768dafc1a1f033
Aman Kumar <amanalphakumar@gmail.com>
2020-09-02 18:25:48 +0530

    Create ring-ed6b505.cpp

---
+++ CodeArena/ring-ed6b505.cpp
@@ -0,0 +1,106 @@
+#### Problem Statement
+
+Today professor Ka has given a string task to Oz and RK. He has given a string STR consisting only of characters '(' and ')' and asked them to convert the string STR into a closed one by adding parenthesis to start and/or end of string STR.
+This means that they could add characters either only to the start of string or only to to the end of string or both.
+
+According to professor, a string is said to be closed if it's each left parenthesis '(' can be paired with a right parenthesis ')' which occurs later in the string.
+As Oz and RK are in hurry beacuse of their other assignments so they wants to add minimum number of parenthesis to start and/or end of string STR to make it a closed one. Help Oz and RK in this string task.
+
+Input:
+First line contain an integer T - number of test cases.
+Each test case contains string STR consisting of characters '(' and ')' only.
+
+Output:
+For each test case output the required string.
+
+Constraints:
+1 ≤ T ≤ 20
+1 ≤ Length Of STR ≤ 103
+SAMPLE INPUT
+
+1
+)(
+
+SAMPLE OUTPUT
+
+()()
+
+Explanation
+
+Add one '(' to the start and one ')' to the end
+
+
+---------
+
+
+#### Solution
+
+
+```
+// amanalphakumar
+
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+#define ll long long int
+
+void fly() ;
+
+int main()
+{
+	ll t = 1 ;
+	cin >> t ;
+
+	while(t--) fly() ;
+
+	return 0 ;
+}
+
+void fly()
+{
+	string str ;
+	cin >> str ;
+
+	int pf = 0, pr = 0  ;
+
+	stack<char> brack ;
+
+	ll i = 0 ;
+
+  // Use stack, to count number of mismatches
+	while( i < str.length() )
+	{
+		if( str[i] == '(' )
+			brack.push( '(' ) ;
+		else
+		{
+			if( !brack.empty() )
+			{
+				brack.pop() ;
+			}
+			else
+			{
+				pf++ ;
+			}
+		}	
+
+		i++ ;
+	}
+
+	pr += brack.size() ;
+
+
+	// Print output
+	while( pf-- )
+		cout << "(" ;
+	
+	cout << str ;
+
+	while( pr-- )
+		cout << ")" ;
+
+	cout << "\n" ;
+
+}
+```
